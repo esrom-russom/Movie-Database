3358280e749a6f2e97bd21e94cf88872256c6c31
esrom-russom <esrom019@gmail.com>
2025-01-30 14:39:22 -0800

    started with header
    Co-authored-by: Robin Badr <robinbadr@users.noreply.github.com>
    Co-authored-by: Yuetong Wang <JW-Designer@users.noreply.github.com>

--- CoolBug/src/components/Header.jsx
+++ CoolBug/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import "./Header.css";
-
+import { APP_TITLE } from "../globals/globals";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Header>
+      <h1>{APP_TITLE}</h1>
+    </Header>
+  );
 }
 
 export default Header;
