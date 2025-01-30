b4a0ca73eea0a1d70b7b3deafa5be950005605b5
esrom-russom <esrom019@gmail.com>
2025-01-30 13:49:26 -0800

    added app router to main
    Co-authored-by: Yuetong Wang <JW-Designer@users.noreply.github.com>
    Co-authored-by: Robin Badr <robinbadr@users.noreply.github.com>

--- CoolBug/src/main.jsx
+++ CoolBug/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import AppRouter from "./Router/AppRouter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>
 );
