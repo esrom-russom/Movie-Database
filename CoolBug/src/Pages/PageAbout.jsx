51be0552a6a397fdb89ea82a45d72d39d7c17bb4
esrom-russom <esrom019@gmail.com>
2025-01-30 13:40:09 -0800

    added css files for the pages Co-authored-by: Yuetong Wang <JW-Designer@users.noreply.github.com>

---
+++ CoolBug/src/Pages/PageAbout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PageAbout.css";
+
+const PageAbout = () => {
+  return (
+    <div className="page-about">
+      <h1>About</h1>
+      {/* Add your about content here */}
+    </div>
+  );
+};
+
+export default PageAbout;
