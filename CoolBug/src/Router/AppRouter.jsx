d27e5d5ecb68d24fc7054671a7840904f53ae13f
esrom-russom <esrom019@gmail.com>
2025-02-04 15:10:19 -0800

    edited on main to add header and footer to router

--- CoolBug/src/Router/AppRouter.jsx
+++ CoolBug/src/Router/AppRouter.jsx
@@ -4,15 +4,18 @@ import PageFavorites from "../Pages/PageFavorites";
 import PageMovie from "../Pages/PageMovie";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
 function AppRouter() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<PageHome />} />
         <Route path="/favorites" element={<PageFavorites />} />
         <Route path="/about" element={<PageAbout />} />
         <Route path="/movie/:id" element={<PageMovie />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
