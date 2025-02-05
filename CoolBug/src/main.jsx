bb3653f402741fc683ad1d06825831f883bbd470
“J” <jytwang0507@gmail.com>
2025-02-05 12:38:51 -0800

    fixed GlobalContext for Favorite button

--- CoolBug/src/main.jsx
+++ CoolBug/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouter from "./Router/AppRouter";
 
+
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppRouter />
   </StrictMode>
 );
+
