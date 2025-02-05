78fd565fa98a773113f50a2bf9fa3a91113de622
esrom-russom <esrom019@gmail.com>
2025-02-05 11:01:46 -0800

    pushing recent changes

--- CoolBug/src/components/FavoriteButton.jsx
+++ CoolBug/src/components/FavoriteButton.jsx
@@ -3,25 +3,25 @@ import { GlobalContext } from "../context/GlobalContext";
 import "./FavoriteButton.css";
 
 function FavoriteButton({ movie }) {
-    const { favorites, addFavorite, removeFavorite } = useContext(GlobalContext);
+  const { favorites, addFavorite, removeFavorite } = useContext(GlobalContext);
 
-    // Determine whether the movie is already in favorites
-    const isFavorite = favorites.some((fav) => fav.id === movie.id);
+  // Determine whether the movie is already in favorites
+  const isFavorite = favorites.some((fav) => fav.id === movie.id);
 
-    function handleClick(event) {
-        event.stopPropagation();
-        if (isFavorite) {
-            removeFavorite(movie);
-        } else {
-            addFavorite(movie);
-        }
+  function handleClick(event) {
+    event.stopPropagation();
+    if (isFavorite) {
+      removeFavorite(movie);
+    } else {
+      addFavorite(movie);
     }
+  }
 
-    return (
-        <button onClick={handleClick}>
-            {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
-        </button>
-    );
+  return (
+    <button onClick={handleClick}>
+      {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+    </button>
+  );
 }
 
 export default FavoriteButton;
