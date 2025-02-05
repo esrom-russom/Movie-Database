308cad808b1abba27df6c2a911d847ef40f41da9
esrom-russom <esrom019@gmail.com>
2025-02-05 12:39:18 -0800

    Chnaged favorite look

--- CoolBug/src/components/FavoriteButton.jsx
+++ CoolBug/src/components/FavoriteButton.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import "./FavoriteButton.css";
+import React from "react";
 
 function FavoriteButton({ movie }) {
   const { favorites, addFavorite, removeFavorite } = useContext(GlobalContext);
 
   // Determine whether the movie is already in favorites
-  const isFavorite = favorites.some((fav) => fav.id === movie.id);
+  const isFavorite = favorites.find((fav) => {
+    return fav.id === movie.id;
+  });
 
   function handleClick(event) {
     event.stopPropagation();
--- CoolBug/src/context/GlobalContext.jsx
+++ CoolBug/src/context/GlobalContext.jsx
@@ -1,37 +1,37 @@
-import {createContext} from "react";
-import {useState,useEffect} from "react";
+import { createContext } from "react";
+import { useState, useEffect } from "react";
 
-const GlobalContext =createContext();
+const GlobalContext = createContext();
 
-function GlobalProvider({children}) {
-    //store movie data objects
-    const[favorites, setFavorites]=useState(loadFavsFromLocalStorage());
+function GlobalProvider({ children }) {
+  //store movie data objects
+  const [favorites, setFavorites] = useState(loadFavsFromLocalStorage());
 
-useEffect(() => {
+  useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favorites));
-}, [favorites]);
+  }, [favorites]);
 
-function loadFavsFromLocalStorage(){
-    const favs=localStorage.getItem("favorites");
+  function loadFavsFromLocalStorage() {
+    const favs = localStorage.getItem("favorites");
     if (favs) {
-        return JSON.parse(favs);
-    }else{
-        return []
+      return JSON.parse(favs);
+    } else {
+      return [];
     }
-}
+  }
 
-function addFavorite(movie){
-    setFavorites([...favorites,movie]);
-}
+  function addFavorite(movie) {
+    setFavorites([...favorites, movie]);
+  }
 
-function removeFavorite(movie){
+  function removeFavorite(movie) {
     setFavorites([favorites.filter((fav) => fav.id !== movie.id)]);
-}
+  }
 
-return (
-    <GlobalContext.Provider value={{favorites,addFavorite,removeFavorite}}>
-        {children}
+  return (
+    <GlobalContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+      {children}
     </GlobalContext.Provider>
-);
+  );
 }
-export {GlobalContext , GlobalProvider};
+export { GlobalContext, GlobalProvider };
