61f6933de7e3025b3007bb4da8cc21cf6b97b5e3
“J” <jytwang0507@gmail.com>
2025-02-14 14:24:14 -0800

    added logo, changed movie card spoacing, added logo in footer

--- CoolBug/src/components/Footer.jsx
+++ CoolBug/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import "./Footer.css";
+import logo from "../assets/logo.svg"; // Adjust the path if needed
 
 function Footer() {
   return (
     <footer>
       <p>
-        <a href="/">MovieDB</a>
+        <a href="/">
+          <img src={logo} alt="MovieDB Logo" className="footer-logo" />
+        </a>
       </p>
       <p>copyright {new Date().getFullYear()}</p>
     </footer>
--- CoolBug/src/components/Header.jsx
+++ CoolBug/src/components/Header.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "../components/react-burger-menu"; // Ensure the import matches the file name
+import logo from "../assets/logo.svg"; // Adjust path as needed
+import favouriteIcon from "../assets/favorite.svg"; // Import the favorite icon
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <HamburgerMenu />
-      <h1 className="logo">
-        <Link to="/">MovieDB</Link>
-      </h1>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} alt="MovieDB Logo" className="logo-img" />
+        </Link>
+      </div>
       <nav className="nav-links">
         <ul>
-          {/* <li>
-            <Link to="/">Home</Link>
-          </li> */}
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <Link to="/favorites" className="nav-item">
+              <img src={favouriteIcon} alt="Favorites" className="nav-icon" />
+              Favorites
+            </Link>
           </li>
           <li>
             <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
-      {/* <HamburgerMenu /> */}
     </header>
   );
 };
